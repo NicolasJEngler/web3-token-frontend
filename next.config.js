d81b60ff321dc15033faa0e8c7c5230f9ac478b1
Nicolás J. Engler <nicojengler@gmail.com>
2022-02-04 08:13:54 -0300

    Implement Web3-token signature request

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   reactStrictMode: true,
+  serverRuntimeConfig: {
+    // Will only be available on the server side
+    // secret: process.env.SECRET
+  },
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+    API_URL: process.env.API_URL,
+  },
 }
